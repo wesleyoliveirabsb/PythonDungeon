f47de250dbe7d2d78835ac1834763b96431bb482
Wesley Oliveira Silva <will.lg@hotmail.com>
2024-06-27 19:49:24 -0300

    dia 1 completo
    prints e inputs

--- Dia1/ex1.py
+++ Dia1/ex1.py
@@ -1,6 +1,30 @@
-print("hello world!")
-print ("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
-print ("2. Knead the dough for 10 minutes.")
-print("3. Add 3g of Salt.")
-print("4. Leave to rise for 2 hours.")
-print("5. Bake at 200 degrees C for 30 minutes.")
+#print("hello world!")
+#print ("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+#print ("2. Knead the dough for 10 minutes.")
+#print("3. Add 3g of Salt.")
+#print("4. Leave to rise for 2 hours.")
+#print("5. Bake at 200 degrees C for 30 minutes.")
+
+#print("A 'single quote' inside a double quote")
+#print('A "double quote" inside a single quote')
+#print("Alternatively you can just \"escape\" the quote")
+
+#print("hello world! \nhello world!")
+#print("hello" + "usuário")
+#print("hello" +" "+ "usuário")
+
+
+#print("Day 1 - String Manipulation")
+#print('String Concatenation is done with the "+" sign.')
+#print('e.g. print("Hello " + "world")')
+#print("New lines can be created with a backslash and n.")
+
+#print("Hello " + input("what is you name?"))
+
+#len signigica lenght e ele calcula o tamanho da proxima string
+name = input()
+nameLen = len(name)
+
+print(nameLen)
+
+input("what is your name?")
