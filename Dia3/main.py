ef9344802ea02f9ac5a26ec89f295f8a1b111d91
Wesley Oliveira Silva <will.lg@hotmail.com>
2024-07-08 21:57:43 -0300

    day3confusion

---
+++ Dia3/main.py
@@ -0,0 +1,8 @@
+print("bem vindo a roda gigante")
+height = int(input("Qual a sua altura em centímetros?"))
+
+if height >= 120 #maior ou igual a 120
+    print("Você pode entrar na roda gigante!!") 
+else:
+    print("Você é pequeno!!!Cresça mais um pouco antes de entrar na roda gigante")
+
